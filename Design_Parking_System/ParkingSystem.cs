9472148175ef5d7e54f529953984a643842ce2d6
Andrew Chebotarev <wizi48wizi@gmail.com>
2025-05-13 15:29:50 +0300

    16, 17

---
+++ Design_Parking_System/ParkingSystem.cs
@@ -0,0 +1,39 @@
+﻿namespace Design_Parking_System
+{
+    public class ParkingSystem
+    {
+        private int big;
+        private int medium;
+        private int small;
+
+        public ParkingSystem(int big, int medium, int small)
+        {
+            this.big = big;
+            this.medium = medium;
+            this.small = small;
+        }
+
+        public bool AddCar(int carType)
+        {
+            if (carType == 1)
+                return ChangeParkingPlaceCount(ref big);
+            else if (carType == 2)
+                return ChangeParkingPlaceCount(ref medium);
+            else if (carType == 3)
+                return ChangeParkingPlaceCount(ref small);
+            else
+                return false;
+        }
+
+        private bool ChangeParkingPlaceCount(ref int parkingPlaceCount)
+        {
+            if (parkingPlaceCount != 0)
+            {
+                parkingPlaceCount--;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ Find_The_Original_Array_of_Prefix_Xor/Find_The_Original_Array_of_Prefix_Xor.cs
@@ -0,0 +1,19 @@
+﻿namespace Find_The_Original_Array_of_Prefix_Xor
+{
+    public class Find_The_Original_Array_of_Prefix_Xor
+    {
+        private int[] resultArray;
+
+        public Find_The_Original_Array_of_Prefix_Xor(int[] pref)
+        {
+            resultArray = new int[pref.Length];
+            resultArray[0] = pref[0];
+
+            for (int i = 1; i < pref.Length; i++)
+                resultArray[i] = pref[i - 1] ^ pref[i];
+
+            foreach (int result in resultArray)
+                Console.WriteLine(result);
+        }
+    }
+}
