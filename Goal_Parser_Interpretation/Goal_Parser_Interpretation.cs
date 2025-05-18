ba040c3479822a45bde0b429ff553f26615691d6
Andrew Chebotarev <wizi48wizi@gmail.com>
2025-05-18 18:53:29 +0300

    18

---
+++ Goal_Parser_Interpretation/Goal_Parser_Interpretation.cs
@@ -0,0 +1,17 @@
+﻿namespace Goal_Parser_Interpretation
+{
+    public class Goal_Parser_Interpretation
+    {
+        public Goal_Parser_Interpretation(string command) 
+        {
+            string commandWithO = GetStringWithO(command);
+            string resultCommand = GetStringWithAL(commandWithO);
+
+            Console.WriteLine(resultCommand);
+        }
+
+        private string GetStringWithO(string command) => command.Replace("()", "o");
+
+        private string GetStringWithAL(string command) => command.Replace("(al)", "al");
+    }
+}
