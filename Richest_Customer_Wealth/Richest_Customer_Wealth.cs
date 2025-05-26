477b0130c2af861f55285bbc2ab94bfbe7e1ad2d
Andrew Chebotarev <wizi48wizi@gmail.com>
2025-05-26 14:46:00 +0300

    19

---
+++ Richest_Customer_Wealth/Richest_Customer_Wealth.cs
@@ -0,0 +1,30 @@
+﻿namespace Richest_Customer_Wealth
+{
+    public class Richest_Customer_Wealth
+    {
+        private int sumRichestClient = 0;
+        private int sumClient = 0;
+
+        public Richest_Customer_Wealth(int[][] accounts)
+        {
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                for (int j = 0; j < accounts[i].Length; j++)
+                    sumClient += accounts[i][j];
+
+                CheckSumRichestClient();
+                ClearSumClient();
+            }
+
+            Console.WriteLine(sumRichestClient.ToString());
+        }
+
+        private void CheckSumRichestClient()
+        {
+            if (sumRichestClient < sumClient)
+                sumRichestClient = sumClient;
+        }
+
+        private void ClearSumClient() => sumClient = 0;
+    }
+}
