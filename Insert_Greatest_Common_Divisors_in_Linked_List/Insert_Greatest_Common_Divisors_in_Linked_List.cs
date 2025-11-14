5f298884800077be95f14ae3f1e77a000e6f3a9c
AndrewChebotarev <wizi48wizi@gmail.com>
2025-11-14 15:32:05 +0300

    38, 39

---
+++ Insert_Greatest_Common_Divisors_in_Linked_List/Insert_Greatest_Common_Divisors_in_Linked_List.cs
@@ -0,0 +1,60 @@
+﻿namespace Insert_Greatest_Common_Divisors_in_Linked_List
+{
+    public class Insert_Greatest_Common_Divisors_in_Linked_List
+    {
+        public Insert_Greatest_Common_Divisors_in_Linked_List(ListNode head)
+        {
+            if (!CheckNullHead(head))
+            {
+                Console.WriteLine(head.ToString());
+                return;
+            }
+
+            if (!CheckSingleHead(head))
+            {
+                Console.WriteLine(head.ToString());
+                return;
+            }
+
+            ListNode current = head;
+
+            while (current != null && current.next != null)
+            {
+                int gcdValue = GCD(current.val, current.next.val);
+
+                ListNode newNode = new(gcdValue);
+
+                newNode.next = current.next;
+                current.next = newNode;
+
+                current = newNode.next;
+            }
+
+            Console.WriteLine(head.ToString());
+        }
+
+        private bool CheckNullHead(ListNode head)
+        {
+            if (head == null) return false;
+            else return true;
+        }
+
+        private bool CheckSingleHead(ListNode head)
+        {
+            if (head.next == null) return false;
+            else return true;
+        }
+
+        private int GCD(int firstValue, int secondValue)
+        {
+            while (secondValue != 0)
+            {
+                int temp = secondValue;
+                secondValue = firstValue % secondValue;
+                firstValue = temp;
+            }
+
+            return firstValue;
+        }
+    }
+}
---
+++ Insert_Greatest_Common_Divisors_in_Linked_List/ListNode.cs
@@ -0,0 +1,31 @@
+﻿//Definition for singly-linked list.
+
+namespace Insert_Greatest_Common_Divisors_in_Linked_List
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        public override string ToString()
+        {
+            List<string> values = new List<string>();
+            ListNode current = this;
+
+            while (current != null)
+            {
+                values.Add(current.val.ToString());
+                current = current.next;
+            }
+
+            return string.Join(" -> ", values) + " -> null";
+        }
+    }
+}
+ 
