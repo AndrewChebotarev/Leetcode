9250136c6640abb958ac2f7392bc2a60c0848073
AndrewChebotarev <wizi48wizi@gmail.com>
2025-11-19 16:04:41 +0300

    40, 41

---
+++ Restore_Finishing_Order/Restore_Finishing_Order.cs
@@ -0,0 +1,22 @@
+﻿namespace Restore_Finishing_Order
+{
+    public class Restore_Finishing_Order
+    {
+        private int counter = 0;
+
+        public Restore_Finishing_Order(int[] order, int[] friends)
+        {
+            int[] result = new int[friends.Length];
+
+            for (int i = 0; i < order.Length; i++)
+                for (int j = 0; j < friends.Length; j++)
+                    if (order[i] == friends[j])
+                    {
+                        result[counter] = friends[j];
+                        counter++;
+                    }
+
+            foreach (int value in result) Console.Write(value + " ");
+        }
+    }
+}
