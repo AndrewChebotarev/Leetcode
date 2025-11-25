537ebb6d89a252ba161084369e129b61de32c791
AndrewChebotarev <wizi48wizi@gmail.com>
2025-11-25 13:48:51 +0300

    42, 43

---
+++ Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cs
@@ -0,0 +1,24 @@
+﻿namespace Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box
+{
+    public class Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box
+    {
+        private int sum = 0;
+        private int[] resultArray;
+
+        public Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box(string boxes)
+        {
+            resultArray = new int[boxes.Length];
+
+            for (int i = 0; i < boxes.Length; i++)
+            {
+                for (int j = 0; j < boxes.Length; j++)
+                    if (boxes[j] == '1') sum += Math.Abs(i - j);
+
+                resultArray[i] = sum;
+                sum = 0;
+            }
+
+            foreach (int resultNumber in resultArray) Console.Write(resultNumber + " ");
+        }
+    }
+}
---
+++ Sum_of_All_Subset_XOR_Totals/Sum_of_All_Subset_XOR_Totals.cs
@@ -0,0 +1,48 @@
+﻿namespace Sum_of_All_Subset_XOR_Totals
+{
+    public class Sum_of_All_Subset_XOR_Totals
+    {
+        private List<List<int>> subsets;
+        private int sum = 0;
+
+        public Sum_of_All_Subset_XOR_Totals(int[] nums)
+        {
+            subsets = GetAllSubsets(nums);
+            XORTotal();
+
+            Console.WriteLine(sum);
+        }
+
+        private List<List<int>> GetAllSubsets(int[] nums)
+        {
+            List<List<int>> subsets = new();
+            subsets.Add(new List<int>());
+
+            foreach (int num in nums)
+            {
+                int count = subsets.Count;
+
+                for (int index = 0; index < count; index++)
+                {
+                    List<int> newSubset = new(subsets[index]);
+
+                    newSubset.Add(num);
+                    subsets.Add(newSubset);
+                }
+            }
+
+            return subsets;
+        }
+
+        private void XORTotal()
+        {
+            foreach (List<int> subset in subsets)
+            {
+                int subsetResult = 0;
+                for (int index = 0; index < subset.Count; index++) subsetResult ^= subset[index];
+
+                sum += subsetResult;
+            }
+        }
+    }
+}
