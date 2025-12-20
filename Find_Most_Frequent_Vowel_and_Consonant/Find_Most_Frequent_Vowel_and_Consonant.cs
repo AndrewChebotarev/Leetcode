5b5766f1d92d4d97053469091c7fbeafbe30a723
AndrewChebotarev <wizi48wizi@gmail.com>
2025-12-20 20:23:50 +0300

    48, 49

---
+++ Find_Most_Frequent_Vowel_and_Consonant/Find_Most_Frequent_Vowel_and_Consonant.cs
@@ -0,0 +1,40 @@
+﻿namespace Find_Most_Frequent_Vowel_and_Consonant
+{
+    public class Find_Most_Frequent_Vowel_and_Consonant
+    {
+        private int vowelsCounter = 0;
+        private int consonantsCounter = 0;
+        private int result = 0;
+
+        private int consonantsChar;
+
+        public Find_Most_Frequent_Vowel_and_Consonant(string s)
+        {
+            foreach (char symbol in s)
+            {
+                if (symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u') ConsistingVowels(symbol, s);
+                else ConsistingConsonants(symbol, s);
+            }
+
+            result = vowelsCounter + consonantsCounter;
+            Console.WriteLine(result);
+        }
+
+        private void ConsistingVowels(char symbol, string s)
+        {
+            int vowelCounter = 0;
+
+            foreach (char inSymbol in s) if (inSymbol == symbol) vowelCounter++;
+            if (vowelsCounter < vowelCounter) vowelsCounter = vowelCounter;
+        }
+
+        private void ConsistingConsonants(char symbol, string s)
+        {
+            int consonantCounter = 0;
+
+            consonantsChar = symbol;
+            foreach (char inSymbol in s) if (consonantsChar == inSymbol) consonantCounter++;
+            if (consonantsCounter < consonantCounter) consonantsCounter = consonantCounter;
+        }
+    }
+}
