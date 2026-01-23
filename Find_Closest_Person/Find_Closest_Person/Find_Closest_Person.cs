118ee3657dcf562983daecb104c0356c70f307a0
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-01-23 15:26:14 +0300

    52, 53

---
+++ Find_Closest_Person/Find_Closest_Person/Find_Closest_Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Find_Closest_Person
+{
+    public class Find_Closest_Person
+    {
+        public Find_Closest_Person(int x, int y, int z) 
+        {
+            int xSteps = Math.Abs(z - x);
+            int ySteps = Math.Abs(z - y);
+
+            if (xSteps < ySteps) Console.WriteLine("1");
+            else if (xSteps > ySteps) Console.WriteLine("2");
+            else Console.WriteLine("0");
+        }
+    }
+}
---
+++ Kids_With_the_Greatest_Number_of_Candies/Kids_With_the_Greatest_Number_of_Candies/Kids_With_the_Greatest_Number_of_Candies.cs
@@ -0,0 +1,30 @@
+﻿namespace Kids_With_the_Greatest_Number_of_Candies
+{
+    public class Kids_With_the_Greatest_Number_of_Candies
+    {
+        private IList<bool> resultList = new List<bool>();
+
+        public Kids_With_the_Greatest_Number_of_Candies(int[] candies, int extraCandies)
+        {
+            for (int index = 0; index < candies.Length; index++)
+            {
+                int temp = candies[index] + extraCandies;
+
+                if (CompareNumbers(candies, temp)) resultList.Add(true);
+                else resultList.Add(false);
+            }
+            
+            foreach (bool result in resultList) Console.Write(result.ToString() + " ");
+
+            Console.WriteLine();
+        }
+
+        private bool CompareNumbers(int[] candies, int number)
+        {
+            for (int index = 0; index < candies.Length; index++)
+                if (number < candies[index]) return false;
+
+            return true;
+        }
+    }
+}
