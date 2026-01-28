882697fcb5f87f5ab95a948b0c265b7416746a37
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-01-28 12:27:38 +0300

    54, 55

---
+++ Minimum_Number_of_Moves_to_Seat_Everyone/Minimum_Number_of_Moves_to_Seat_Everyone.cs
@@ -0,0 +1,17 @@
+﻿namespace Minimum_Number_of_Moves_to_Seat_Everyone
+{
+    public class Minimum_Number_of_Moves_to_Seat_Everyone
+    {
+        private int resultSteps = 0;
+
+        public Minimum_Number_of_Moves_to_Seat_Everyone(int[] seats, int[] students)
+        {
+            Array.Sort(seats);
+            Array.Sort(students);
+
+            for (int i = 0; i < seats.Length; i++) resultSteps += Math.Abs(seats[i] - students[i]);
+
+            Console.WriteLine(resultSteps);
+        }
+    }
+}
---
+++ Number_of_Laser_Beams_in_a_Bank/Number_of_Laser_Beams_in_a_Bank.cs
@@ -0,0 +1,39 @@
+﻿namespace Number_of_Laser_Beams_in_a_Bank
+{
+    public class Number_of_Laser_Beams_in_a_Bank
+    {
+        private int result = 0;
+        private List<string> bankList;
+
+        public Number_of_Laser_Beams_in_a_Bank(string[] bank)
+        {
+            CreateNewBankList(bank);
+
+            if (CheckNullResult()) Console.WriteLine(result);
+            else GetResult();
+
+            Console.WriteLine(result);
+        }
+
+        private void CreateNewBankList(string[] bank)
+        {
+            bankList = new List<string>();
+
+            for (int index = 0; index < bank.Length; index++)
+                if (bank[index].Contains('1')) bankList.Add(bank[index]);
+        }
+
+        private bool CheckNullResult() => bankList.Count == 0;
+
+        private void GetResult()
+        {
+            for (int index = 0; index < bankList.Count - 1; index++)
+            {
+                int countCharFirst = bankList[index].Count(c => c == '1');
+                int countCharSecond = bankList[index + 1].Count(c => c == '1');
+
+                result += countCharFirst * countCharSecond;
+            }
+        }
+    }
+}
