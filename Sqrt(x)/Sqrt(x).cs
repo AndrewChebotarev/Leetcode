04e70cf23c8c4a3ae7332310b818eed978dd7f3f
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-02-17 16:11:50 +0300

    61

---
+++ Sqrt(x)/Sqrt(x).cs
@@ -0,0 +1,13 @@
+﻿namespace Sqrt_x_
+{
+    public class Sqrt_x_
+    {
+        public Sqrt_x_(int x)
+        {
+            int result = 0;
+            for (long num = 1; (num * num) <= x; num++) result = (int)num;
+
+            Console.WriteLine(result);
+        }
+    }
+}
