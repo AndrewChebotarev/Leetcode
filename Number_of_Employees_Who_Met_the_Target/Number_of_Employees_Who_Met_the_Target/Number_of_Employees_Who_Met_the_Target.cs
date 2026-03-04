ff819515caf272166a3b715d601b7839359a9ad3
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-03-04 12:00:46 +0300

    63

---
+++ Number_of_Employees_Who_Met_the_Target/Number_of_Employees_Who_Met_the_Target/Number_of_Employees_Who_Met_the_Target.cs
@@ -0,0 +1,11 @@
+﻿namespace Number_of_Employees_Who_Met_the_Target
+{
+    public class Number_of_Employees_Who_Met_the_Target
+    {
+        public Number_of_Employees_Who_Met_the_Target(int[] hours, int target)
+        {
+            int result = hours.Where(employee => employee >= target).Count();
+            Console.WriteLine(result);
+        }
+    }
+}
