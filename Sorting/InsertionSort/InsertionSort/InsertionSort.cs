b5769bb9d0cb920269a94443d8088fcb4b11e82e
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-03-06 13:47:15 +0300

    InsertionSort

---
+++ Sorting/InsertionSort/InsertionSort/InsertionSort.cs
@@ -0,0 +1,24 @@
+﻿namespace InsertionSort
+{
+    public class InsertionSort
+    {
+        public InsertionSort(int[] array) 
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int currentNumber = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > currentNumber)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                array[j + 1] = currentNumber;
+            }
+
+            foreach (int number in array) Console.Write(number + " ");
+        }
+    }
+}
---
+++ Sorting/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace InsertionSort
+{
+    public class Program
+    {
+        private static int[] testArray = { 6, 3, 8, 9, 2, 5 };
+        public static void Main(string[] args)
+        {
+            InsertionSort insertionSort = new(testArray);
+        }
+    }
+}
