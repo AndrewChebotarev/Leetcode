04e3cb984773fb682739302a038ea31b8bb0fafe
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-03-06 10:06:54 +0300

    BubbleSort

---
+++ Sorting/BubbleSort/BubbleSort/BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿namespace BubbleSort
+{
+    public class BubbleSort
+    {
+        private bool isSorted = false;
+
+        public BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                isSorted = false;
+
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+
+                        isSorted = true;
+                    }
+                }
+
+                if (!isSorted) break;
+            }
+
+            foreach (int number in array) Console.Write(number + " ");
+        }
+    }
+}
---
+++ Sorting/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BubbleSort
+{
+    public class Program
+    {
+        private static int[] testArray = { 6, 3, 2, 8, 1, 9 };
+        public static void Main(string[] args)
+        {
+            BubbleSort bubbleSort = new(testArray);
+        }
+    }
+}
