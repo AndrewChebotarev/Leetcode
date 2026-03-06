a510a5c2958542591177c8a058e24d371dc52f2f
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-03-06 09:34:19 +0300

    64

---
+++ Maximum_Number_of_Words_Found_in_Sentences/Maximum_Number_of_Words_Found_in_Sentences/Maximum_Number_of_Words_Found_in_Sentences.cs
@@ -0,0 +1,25 @@
+﻿namespace Maximum_Number_of_Words_Found_in_Sentences
+{
+    public class Maximum_Number_of_Words_Found_in_Sentences
+    {
+        private int maxNumberWords = 0;
+        private int wordsInSentence = 0;
+
+        public Maximum_Number_of_Words_Found_in_Sentences(string[] sentences)
+        {
+            foreach (string sentence in sentences)
+            {
+                GetWordsInSentence(sentence);
+                CheckMaxNumberWords();
+            }
+            Console.WriteLine(maxNumberWords);
+        }
+
+        private void GetWordsInSentence(string sentence) => wordsInSentence = sentence.Split(new char[] { ' ' }).Length;
+
+        private void CheckMaxNumberWords()
+        {
+            if (wordsInSentence > maxNumberWords) maxNumberWords = wordsInSentence;
+        }
+    }
+}
