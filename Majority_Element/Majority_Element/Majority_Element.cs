349f07fa06860914436e6e3905648d2d9cea3243
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-03-16 16:20:55 +0300

    65

---
+++ Majority_Element/Majority_Element/Majority_Element.cs
@@ -0,0 +1,24 @@
+﻿namespace Majority_Element
+{
+    public class Majority_Element
+    {
+        private int numberCount = 0;
+        public Majority_Element(int[] nums) 
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j]) numberCount++;
+                    if (numberCount > nums.Length / 2)
+                    {
+                        Console.WriteLine(nums[i]);
+                        return;
+                    }
+                }
+                numberCount = 0;
+            }
+            Console.WriteLine("Error");
+        }
+    }
+}
