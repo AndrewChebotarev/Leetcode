7bd4a8c9659456fdf614e6b5473d0e75c9615663
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-04-29 14:59:49 +0300

    68

---
+++ Decompress_Run-Length_Encoded_List/Decompress_Run-Length_Encoded_List/Decompress_Run-Length_Encoded_List.cs
@@ -0,0 +1,20 @@
+﻿namespace Decompress_Run_Length_Encoded_List
+{
+    public class Decompress_Run_Length_Encoded_List
+    {
+        private int[] result;
+        private List<int> resultList;
+        public Decompress_Run_Length_Encoded_List(int[] nums) 
+        {
+            resultList = new List<int>();
+
+            for (int i = 0; i < nums.Length; i = i + 2)
+                for (int j = 0; j < nums[i]; j++)
+                    resultList.Add(nums[i + 1]);
+
+            result = resultList.ToArray();
+
+            foreach (int num in result) Console.Write(num + " ");
+        }
+    }
+}
