578517089a9012df76569c94a32ecb043ba0460c
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-04-29 14:39:16 +0300

    BucketSort

---
+++ Sorting/BucketSort/BucketSort/BucketSort.cs
@@ -0,0 +1,47 @@
+﻿namespace BucketSort
+{
+    public class BucketSort
+    {
+        private int[] array;
+        public BucketSort(int[] array) 
+        {
+            this.array = array;
+            Sort(this.array);
+
+            foreach(int num in array) Console.Write(num + " ");
+        }
+
+        public void Sort(int[] array, int numberOfBuckets = 10)
+        {
+            if (array == null || array.Length <= 1) return;
+
+            int min = array[0];
+            int max = array[0];
+            foreach (int num in array)
+            {
+                if (num < min) min = num;
+                if (num > max) max = num;
+            }
+
+            List<int>[] buckets = new List<int>[numberOfBuckets];
+            for (int i = 0; i < numberOfBuckets; i++) buckets[i] = new List<int>();
+
+            double range = (double)(max - min) / numberOfBuckets;
+
+            foreach (int num in array)
+            {
+                int bucketIndex = (int)Math.Floor((num - min) / range);
+                if (bucketIndex >= numberOfBuckets) bucketIndex = numberOfBuckets - 1;
+
+                buckets[bucketIndex].Add(num);
+            }
+
+            int currentIndex = 0;
+            foreach (var bucket in buckets)
+            {
+                bucket.Sort();
+                foreach (int num in bucket) array[currentIndex++] = num;
+            }
+        }
+    }
+}
---
+++ Sorting/BucketSort/BucketSort/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BucketSort
+{
+    public class Program
+    {
+        private static int[] array = { 42, 17, 89, 3, 56, 91, 23, 8, 67, 34, 78, 12 };
+        public static void Main(string[] args)
+        {
+            BucketSort bucketSort = new(array);
+        }
+    }
+}
