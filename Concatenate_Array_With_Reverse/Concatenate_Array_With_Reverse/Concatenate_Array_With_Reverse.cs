491edbf65567a87e21fbff683d1721b8c65e9835
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-05-28 13:58:49 +0300

    70

---
+++ Concatenate_Array_With_Reverse/Concatenate_Array_With_Reverse/Concatenate_Array_With_Reverse.cs
@@ -0,0 +1,18 @@
+﻿namespace Concatenate_Array_With_Reverse
+{
+    public class Concatenate_Array_With_Reverse
+    {
+        public Concatenate_Array_With_Reverse(int[] nums)
+        {
+            int[] result = new int[nums.Length * 2];
+
+            for (int index = 0; index < nums.Length; index++)
+            {
+                result[index] = nums[index];
+                result[index + nums.Length] = nums[(nums.Length - 1) - index];
+            }
+
+            foreach (int num in result) Console.Write(num + " ");
+        }
+    }
+}
