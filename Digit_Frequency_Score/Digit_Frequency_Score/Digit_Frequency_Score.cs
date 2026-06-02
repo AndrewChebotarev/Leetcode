a6812a6725f040221ea41989c8f716eb790803b9
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-06-02 14:58:40 +0300

    71

---
+++ Digit_Frequency_Score/Digit_Frequency_Score/Digit_Frequency_Score.cs
@@ -0,0 +1,16 @@
+﻿namespace Digit_Frequency_Score
+{
+    public class Digit_Frequency_Score
+    {
+        private int result = 0;
+        private string nString = "";
+
+        public Digit_Frequency_Score(int n)
+        {
+            nString = n.ToString();
+            foreach (char sumbol in nString) result += int.Parse(sumbol.ToString());
+
+            Console.WriteLine(result);
+        }
+    }
+}
