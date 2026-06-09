591d13b5a3ec215f94b5ff2308cd422adda65443
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-06-09 14:38:53 +0300

    BinarySearch

---
+++ Searching/BinarySearch/BinarySearch/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿namespace BinarySearch
+{
+    public class BinarySearch
+    {
+        public int Searching(int[] array, int target)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (array[mid] == target) return mid;
+                else if (array[mid] < target) left = mid + 1;
+                else right = mid - 1;
+            }
+
+            return -1;
+        }
+
+        public int RecursiveSearching(int[] array, int target, int left, int right)
+        {
+            if (left > right) return -1;
+
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) return mid;
+            else if (array[mid] < target) return RecursiveSearching(array, target, mid + 1, right);
+            else return RecursiveSearching(array, target, left, mid - 1);
+        }
+    }
+}
---
+++ Searching/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BinarySearch
+{
+    public class Program
+    {
+        private static int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        public static void Main(string[] args)
+        {
+            BinarySearch binarySearch = new();
+
+            int result = binarySearch.Searching(array, 3);
+            Console.WriteLine("Результат обычного бинарного поиска = " + result);
+
+            result = binarySearch.RecursiveSearching(array, 6, 0, array.Length - 1);
+            Console.WriteLine("Результат рекурсивного бинарного поиска = " + result);
+        }
+    }
+}
