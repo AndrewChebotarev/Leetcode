ce00ff16efbb815e404edab5a786f92d9f61772a
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-06-11 17:35:35 +0300

    73

---
+++ Reverse_Degree_of_a_String/Reverse_Degree_of_a_String/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+3498. Reverse Degree of a String
+
+Given a string s, calculate its reverse degree.
+
+The reverse degree is calculated as follows:
+
+For each character, multiply its position in the reversed alphabet ('a' = 26, 'b' = 25, ..., 'z' = 1) with its position in the string (1-indexed).
+Sum these products for all characters in the string.
+Return the reverse degree of s.
+
+Example 1:
+
+Input: s = "abc"
+
+Output: 148
+
+Explanation:
+
+Letter	Index in Reversed Alphabet	Index in String	Product
+'a'	26	1	26
+'b'	25	2	50
+'c'	24	3	72
+The reversed degree is 26 + 50 + 72 = 148.
+
+Example 2:
+
+Input: s = "zaza"
+
+Output: 160
+
+Explanation:
+
+Letter	Index in Reversed Alphabet	Index in String	Product
+'z'	1	1	1
+'a'	26	2	52
+'z'	1	3	3
+'a'	26	4	104
+The reverse degree is 1 + 52 + 3 + 104 = 160.
+
+Constraints:
+
+1 <= s.length <= 1000
+s contains only lowercase English letters.
+ */
+namespace Reverse_Degree_of_a_String
+{
+    public class Program
+    {
+        private static string s1 = "abc";
+        private static string s2 = "zaza";
+
+        public static void Main(string[] args)
+        {
+            Reverse_Degree_of_a_String reverse_Degree_Of_A_String = new();
+
+            int result = reverse_Degree_Of_A_String.ReverseDegree(s1);
+            Console.WriteLine(result);
+
+            result = reverse_Degree_Of_A_String.ReverseDegree(s2);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Reverse_Degree_of_a_String/Reverse_Degree_of_a_String/Reverse_Degree_of_a_String.cs
@@ -0,0 +1,24 @@
+﻿namespace Reverse_Degree_of_a_String
+{
+    public class Reverse_Degree_of_a_String
+    {
+        private Dictionary<char, int> symbols = new()
+        {
+            { 'a', 26 }, { 'b', 25 }, { 'c', 24 }, { 'd', 23 }, { 'e', 22 }, { 'f', 21 },
+            { 'g', 20 }, { 'h', 19 }, { 'i', 18 }, { 'j', 17 }, { 'k', 16 }, { 'l', 15 },
+            { 'm', 14 }, { 'n', 13 }, { 'o', 12 }, { 'p', 11 }, { 'q', 10 }, { 'r', 9 },
+            { 's', 8 }, { 't', 7 }, { 'u', 6 }, { 'v', 5 }, { 'w', 4 }, { 'x', 3 }, { 'y', 2 },
+            { 'z', 1 }
+        };
+
+        public int ReverseDegree(string s)
+        {
+            int result = 0;
+
+            for (int index = 0; index < s.Length; index++)
+                result += symbols[s[index]] * (index + 1);
+
+            return result;
+        }
+    }
+}
