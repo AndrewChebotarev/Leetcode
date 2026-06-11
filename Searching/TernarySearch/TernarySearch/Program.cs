7f2bbc402d0d2b2d6012823043e5d0fee610df59
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-06-11 17:13:36 +0300

    Ternary Search

---
+++ Searching/TernarySearch/TernarySearch/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TernarySearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TernarySearch ternarySearch = new TernarySearch();
+
+            double result = ternarySearch.FindMinimum(FunctionForMin, -100, 100);
+            Console.WriteLine("Тернарынй оператор (обычный минимум) = " + result);
+
+            result = ternarySearch.FindMaximum(FunctionForMax, -100, 100);
+            Console.WriteLine("Тернарынй оператор (обычный максимум) = " + result);
+
+            result = ternarySearch.FindMinimumRecursive(FunctionForMin, -100, 100);
+            Console.WriteLine("Тернарынй оператор (рекурсивный минимум) = " + result);
+
+            result = ternarySearch.FindManimumRecursive(FunctionForMax, -100, 100);
+            Console.WriteLine("Тернарынй оператор (рекурсивный максимум) = " + result);
+        }
+
+        private static double FunctionForMin(double x)
+        {
+            return (x - 2) * (x - 2) + 1;
+        }
+
+        private static double FunctionForMax(double x)
+        {
+            return -(x - 2) * (x - 2) + 10;
+        }
+    }
+}
---
+++ Searching/TernarySearch/TernarySearch/TernarySearch.cs
@@ -0,0 +1,61 @@
+﻿namespace TernarySearch
+{
+    public class TernarySearch
+    {
+        public double FindMinimum(Func<double, double> function, double left, double right, double epsilon = 1e-9)
+        {
+            while (right - left > epsilon)
+            {
+                double m1 = left + (right - left) / 3;
+                double m2 = right - (right - left) / 3;
+
+                double f1 = function(m1);
+                double f2 = function(m2);
+
+                if (f1 < f2) right = m2;
+                else left = m1;
+            }
+
+            return (left + right) / 2;
+        }
+
+        public double FindMaximum(Func<double, double> function, double left, double right, double epsilon = 1e-9)
+        {
+            while (right - left > epsilon)
+            {
+                double m1 = left + (right - left) / 3;
+                double m2 = right - (right - left) / 3;
+
+                double f1 = function(m1);
+                double f2 = function(m2);
+
+                if (f1 > f2) right = m2;
+                else left = m1;
+            }
+
+            return (left + right) / 2;
+        }
+
+        public double FindMinimumRecursive(Func<double, double> function,  double left, double right, double epsilon = 1e-9)
+        {
+            if (right - left <= epsilon) return (left + right) / 2;
+
+            double m1 = left + (right - left) / 3;
+            double m2 = right - (right - left) / 3;
+
+            if (function(m1) < function(m2)) return FindMinimumRecursive(function, left, m2, epsilon);
+            else return FindMinimumRecursive(function, m1, right, epsilon);
+        }
+
+        public double FindManimumRecursive(Func<double, double> function, double left, double right, double epsilon = 1e-9)
+        {
+            if (right - left <= epsilon) return (left + right) / 2;
+
+            double m1 = left + (right - left) / 3;
+            double m2 = right - (right - left) / 3;
+
+            if (function(m1) > function(m2)) return FindManimumRecursive(function, left, m2, epsilon);
+            else return FindManimumRecursive(function, m1, right, epsilon);
+        }
+    }
+}
