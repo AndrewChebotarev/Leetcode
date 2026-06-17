b10eff76841b3febf689aba59c7fe48e92472004
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-06-17 19:20:02 +0300

    Quick Select

---
+++ Searching/QuickSelect/QuickSelect/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace QuickSelect
+{
+    public class Program
+    {
+        private static int[] array = { 70, 20, 90, 10, 60, 30, 80, 50, 40 };
+        public static void Main(string[] args)
+        {
+            QuickSelect quickSelect = new();
+            int result = quickSelect.FindKthSmallest(array, 5);
+            Console.WriteLine("Итеративный поиск: " + result);
+
+            result = quickSelect.FindKthSmallestRecursive(array, 6);
+            Console.WriteLine("Рекурсивный поиск: " + result);
+
+            result = quickSelect.FindKthSmallestReadOnly(array, 4);
+            Console.WriteLine("Поиск без изменения массива: " + result);
+        }
+    }
+}
---
+++ Searching/QuickSelect/QuickSelect/QuickSelect.cs
@@ -0,0 +1,100 @@
+﻿namespace QuickSelect
+{
+    public class QuickSelect
+    {
+        private static Random rand = new Random();
+
+        public int FindKthSmallest(int[] array, int k)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int pivotIndex = rand.Next(left, right + 1);
+                Swap(array, pivotIndex, right);
+
+                int storeIndex = left;
+                for (int i = left; i < right; i++)
+                {
+                    if (array[i] <= array[right])
+                    {
+                        Swap(array, storeIndex, i);
+                        storeIndex++;
+                    }
+                }
+
+                Swap(array, storeIndex, right);
+
+                if (storeIndex == k) return array[storeIndex];
+                else if (storeIndex > k) right = storeIndex - 1;
+                else left = storeIndex + 1;
+            }
+
+            return -1;
+        }
+
+        public int FindKthSmallestRecursive(int[] array, int k) => RecursiveMethod(array, 0, array.Length - 1, k);
+
+        private int RecursiveMethod(int[] array, int left, int right, int k)
+        {
+            if (left == right) return array[left];
+
+            int pivotIndex = rand.Next(left, right + 1);
+            Swap(array, pivotIndex, right);
+
+            int storeIndex = left;
+            for (int i = left; i < right; i++)
+            {
+                if (array[i] <= array[right])
+                {
+                    Swap(array, storeIndex, i);
+                    storeIndex++;
+                }
+            }
+            Swap(array, storeIndex, right);
+
+            if (storeIndex == k) return array[storeIndex];
+            else if (storeIndex > k) return RecursiveMethod(array, left, storeIndex - 1, k);
+            else return RecursiveMethod(array, storeIndex + 1, right, k);
+        }
+
+        public int FindKthSmallestReadOnly(int[] originalArray, int k)
+        {
+            int[] array = (int[])originalArray.Clone();
+
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int pivotIndex = rand.Next(left, right + 1);
+                Swap(array, pivotIndex, right);
+
+                int storeIndex = left;
+                for (int i = left; i < right; i++)
+                {
+                    if (array[i] <= array[right])
+                    {
+                        Swap(array, storeIndex, i);
+                        storeIndex++;
+                    }
+                }
+                Swap(array, storeIndex, right);
+
+                if (storeIndex == k) return array[storeIndex];
+                else if (storeIndex > k) right = storeIndex - 1;
+                else left = storeIndex + 1;
+            }
+
+            return -1;
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
