7fc336aa66a6b3cf1ea6c7dd55d2d45f8fc6c482
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-06-17 19:46:27 +0300

    74

---
+++ Permutation_Difference_between_Two_Strings/Permutation_Difference_between_Two_Strings/Permutation_Difference_between_Two_Strings.cs
@@ -0,0 +1,16 @@
+﻿namespace Permutation_Difference_between_Two_Strings
+{
+    public class Permutation_Difference_between_Two_Strings
+    {
+        public int FindPermutationDifference(string s, string t)
+        {
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+                for (int j = 0; j < t.Length; j++)
+                    if (s[i] == t[j]) result += Math.Abs(i - j);
+
+            return result;
+        }
+    }
+}
