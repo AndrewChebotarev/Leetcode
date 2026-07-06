f82ce30663ace2fde3ac4eec5c720bf923cc1f0b
Andrew Chebotarev <wizi48wizi@gmail.com>
2026-07-06 19:44:54 +0300

    75

---
+++ Shuffle_the_Array/Shuffle_the_Array/Shuffle_the_Array.cs
@@ -0,0 +1,27 @@
+﻿namespace Shuffle_the_Array
+{
+    public class Shuffle_the_Array
+    {
+        private int[] result;
+
+        public int[] Shuffle(int[] nums, int n)
+        {
+            result = new int[nums.Length];
+
+            SetX(nums);
+            SetY(nums, n);
+
+            return result;
+        }
+
+        private void SetX(int[] nums)
+        {
+            for (int i = 0, j = 0; i < nums.Length; i += 2, j++) result[i] = nums[j];
+        }
+
+        private void SetY(int[] nums, int n)
+        {
+            for (int i = 1, j = n; i < nums.Length; i += 2, j++) result[i] = nums[j];
+        }
+    }
+}
